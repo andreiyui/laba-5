53c9929bb6582078390a76c1dc620d219cddc06e
andreiyui <147510433+andreiyui@users.noreply.github.com>
2023-10-27 11:02:51 +0300

    Add files via upload

---
+++ Проект 4 (5).cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+    setlocale(0, "");
+    int arr[] = { 1, 2, 3, 4, 5 };
+
+    int size = sizeof(arr) / sizeof(arr[0]);
+    int sum = 0;
+
+    for (int i = 0; i < size; i++) {
+        sum += arr[i];
+    }
+
+    double average = static_cast<double>(sum) / size;
+
+    cout << "Среднее арифметическое: " << average << endl;
+
+    return 0;
+}
---
+++ Проект 5 (5).cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+int main() {
+    setlocale(0, "");
+    int x1, x2;
+    int a = 0, b = 0, c = 0, d = 0;
+    cout << "Введите коэффициэнт :\n";
+    cin >> a >> b >> c;
+    d = (b * b) - (4 * a * c);
+    if (d == 0) {
+        x1 = (-b + sqrt(d)) / (2 * a);
+        cout << "x : =" << x1 << endl;
+    }
+    else if (d > 0) {
+        x1 = (-b + sqrt(d)) / (2 * a);
+        x2 = (-b - sqrt(d)) / (2 * a);
+        cout << "x1 : =" << x1 << endl;
+        cout << "x2 : =" << x2 << endl;
+    }
+    else if (d < 0) {
+        cout << "корней нет" << endl;
+    }
+    else {
+        cout << "i love Chief Keef";
+    }
+
+    return 0;
+}
